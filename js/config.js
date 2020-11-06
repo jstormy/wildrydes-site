ffcad153ce30a232242e38618c5732f53a962722
jstormy <54546304+jstormy@users.noreply.github.com>
2020-11-05 21:23:44 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_QPOXqpu09',
         userPoolClientId: '1olkni8as94rui2nn81s4vbmrk',
-        region: 'eu-west-1'
+        region: 'us-east-1'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
